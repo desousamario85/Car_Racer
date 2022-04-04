2e687f2c062b2cec3190d9929dcd94e193570eef
desousamario85 <desousa.mario@yahoo.com>
2022-04-04 20:52:09 +0000

    Imported termcolor to enable colour text

--- run.py
+++ run.py
@@ -1,12 +1,20 @@
+
+# Importing required modules into the project
+
 import gspread
 from google.oauth2.service_account import Credentials
+import sys
+from termcolor import colored, cprint
 
+# Standard Google Drive Scope to access files in our google drive.
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
     ]
+
+# Credentials to authenticate to the Leader_Board file
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
